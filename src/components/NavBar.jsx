baf0a44d8c5127c2d17dc2ab67239e3206a0eff8
vps07 <vikysingh07860@gmail.com>
2023-02-13 21:05:44 +0530

    fix some navbar issue

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -19,16 +19,23 @@ export default function NavBar() {
 
     return (
         <div className="bg-blue-600 w-[100%] h-[50px] pt-2 fixed">
-            <div className="flex items-center">
-                <Link to="/" className="flex text-white space-x-2 ml-16">
-                    <BsShop className="text-3xl" />
-                    <h3 className="text-xl mt-1 font-bold">Fliss Store</h3>
-                </Link>
-                <div className="relative ml-7">
-                    <input type="text" value={txtInput} placeholder="Search for products" className="outline-none py-1 px-3 rounded-sm w-96 lg:w-[500px] focus:ring" onChange={onChangeHandler} />
-                    <BiSearch className="absolute left-[22rem] lg:left-[470px] bottom-[0.35rem] text-blue-600 text-xl" />
+            <div className="flex items-center justify-between">
+                <div className="flex">
+                    {/* Shopping cart title */}
+                    <Link to="/" className="flex text-white space-x-2 ml-12">
+                        <BsShop className="text-3xl" />
+                        <h3 className="text-base md:text-xl mt-1 font-bold">Fliss Store</h3>
+                    </Link>
+
+                    {/* Product serach bar */}
+                    <div className="relative ml-12">
+                        <input type="text" value={txtInput} placeholder="Search for products" className="outline-none py-1 px-3 rounded-sm w-96 md:w-[500px] focus:ring" onChange={onChangeHandler} />
+                        <BiSearch className="absolute left-[22rem] md:left-[470px] bottom-[0.35rem] text-blue-600 text-xl" />
+                    </div>
                 </div>
-                <div className="text-white flex ml-24 cursor-pointer lg:ml-[700px] active:bg-blue-700 active:p-[0.2rem] active:rounded">
+
+                {/* Shopping cart button */}
+                <div className="text-white flex cursor-pointer mr-7 active:bg-blue-700 active:p-[0.2rem] active:rounded">
                     <Link to="/cart" className="flex">
                         <MdAddShoppingCart className="text-2xl" />
                         <p className="text-[0.6rem] mt-2 bg-red-500 rounded-md px-1 py-[0.1] text-center font-bold -ml-1">{itemsInCart.length}</p>
